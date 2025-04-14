6d3fc0a430d0aa55f96418463770bf382f77498c
Shub Raj Lama <hello@shubraj.com>
2025-04-14 18:56:39 +0545

    init

--- schedule_the_app.bat
+++ schedule_the_app.bat
@@ -1,9 +1,8 @@
 @echo off
 REM Setup script to run FastAPI app at system startup
-
 REM Get full script directory and app path
 set "SCRIPT_DIR=%~dp0"
-set "SCRIPT_PATH=%SCRIPT_DIR%app.py"
+set "APP_PATH=%SCRIPT_DIR%app.py"
 set "TASK_NAME=FastAPIRecorderOnStartup"
 
 REM Check for Python installation
@@ -21,8 +20,23 @@ if %errorlevel% == 0 (
     )
 )
 
-REM Create the task with absolute app.py path
-schtasks /create /f /sc onstart /tn "%TASK_NAME%" /tr "\"%PYTHON_PATH%\" -m uvicorn app:app --host 127.0.0.1 --port 8000" /rl HIGHEST
+REM Get the full path to Python executable
+for /f "tokens=*" %%i in ('where %PYTHON_PATH%') do set "PYTHON_FULL_PATH=%%i"
+
+REM Create the task with required parameters:
+REM - Working directory set to script location
+REM - Run with highest privileges
+REM - Run task as soon as possible after a scheduled start is missed
+REM - Don't require battery power
+schtasks /create /f /sc onstart /tn "%TASK_NAME%" /tr "\"%PYTHON_FULL_PATH%\" \"%APP_PATH%\"" /rl HIGHEST /st 00:00 /ru SYSTEM /np /delay 0000:01 /v1 /z
+
+REM Additional XML modifications for battery settings and missed start
+powershell -Command "$xml = [xml](schtasks /query /tn '%TASK_NAME%' /xml); $xml.Task.Settings.DisallowStartIfOnBatteries = 'false'; $xml.Task.Settings.RunOnlyIfIdle = 'false'; $xml.Task.Settings.StartWhenAvailable = 'true'; $xml.Save('%TEMP%\task.xml'); schtasks /create /f /tn '%TASK_NAME%' /xml '%TEMP%\task.xml'"
 
 echo ✅ Task Scheduler job '%TASK_NAME%' created to run on system startup!
+echo    - Working directory: %SCRIPT_DIR%
+echo    - Python path: %PYTHON_FULL_PATH%
+echo    - App path: %APP_PATH%
+echo    - Will run as soon as possible after missed start
+echo    - Will run regardless of battery status
 pause
